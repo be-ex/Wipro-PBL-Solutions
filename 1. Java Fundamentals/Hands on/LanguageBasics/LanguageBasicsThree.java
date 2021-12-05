6cedf97f1068a09f2caefc947a55009ae6d7d48d
be-ex <95574946+be-ex@users.noreply.github.com>
2021-12-05 19:43:38 +0530

    Delete 1. Java Fundamentals directory

--- 1. Java Fundamentals/Hands on/LanguageBasics/LanguageBasicsThree.java
+++
@@ -1,6 +0,0 @@
-public class LanguageBasicsThree {
-    public static void main(String[] args) {
-        int result= Integer.parseInt(args[0])+Integer.parseInt(args[1]);
-        System.out.println("The sum of "+args[0]+" and "+args[1]+" is "+ result);
-    }
-}
--- 1. Java Fundamentals/Hands on/LanguageBasics/LanguageBasicsTwo.java
+++
@@ -1,5 +0,0 @@
-public class LanguageBasicsTwo {
-    public static void main(String[] args) {
-        System.out.println("Welcome "+args[0]);
-    }
-}
