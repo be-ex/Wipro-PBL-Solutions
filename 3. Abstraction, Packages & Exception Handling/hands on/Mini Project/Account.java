e2da4940b5b42a0aba348bc927d418d63b8d0883
be-ex <95574946+be-ex@users.noreply.github.com>
2021-12-17 18:16:11 +0530

    Add files via upload

---
+++ 3. Abstraction, Packages & Exception Handling/hands on/Mini Project/Account.java
@@ -0,0 +1,26 @@
+
+
+public abstract class Account {
+
+	public double interestRate;
+	public double amount;
+
+	abstract double calculateInterest();
+
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+}
